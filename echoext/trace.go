e546ef063f70b49fe606e642e07c075f47d4be71
lucas <lucas@yeeuu.com>
2019-05-21 09:51:22 +0800

    更新echoext,logext日志

--- echoext/trace.go
+++ echoext/trace.go
@@ -2,32 +2,41 @@ package echoext
 
 import (
 	"context"
+	"strings"
 
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
 
 // ExtStdContext 扩展标准context.Context信息，方便后续Logger使用
-func ExtStdContext(fn func(c echo.Context) interface{}) echo.MiddlewareFunc {
+func ExtStdContext() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			ctx := c.Request().Context()
-			reqID := c.Request().Header.Get("X-Request-ID")
-			if reqID == "" {
-				reqID = uuid.New().String()
+			reqID := ""
+
+			xreq := c.Get(echo.HeaderXRequestID)
+			if xreq == nil {
+				reqID = generateUuid()
+			} else if xreq == "" {
+				reqID = generateUuid()
 			}
-			var id interface{}
-			if fn != nil {
-				id = fn(c)
-			}
-			ctx = context.WithValue(ctx, "logger_client_ip", c.RealIP())
-			ctx = context.WithValue(ctx, "logger_request_id", reqID)
-			ctx = context.WithValue(ctx, "logger_unique_id", id)
+			ctx = context.WithValue(ctx, "ClientIP", c.RealIP())
+			ctx = context.WithValue(ctx, "RequestID", reqID)
+			ctx = context.WithValue(ctx, "DentifyID", c.Get("userId"))
+			ctx = context.WithValue(ctx, "ServersID", "mountain_go")
 			req := c.Request().WithContext(ctx)
 			c.SetRequest(req)
 			err := next(c)
-			c.Response().Header().Set("X-Request-ID", reqID)
+			c.Response().Header().Set(echo.HeaderXRequestID, reqID)
 			return err
 		}
 	}
+}
+
+func generateUuid() (id string) {
+	rawId := uuid.New().String()
+	Ids := strings.Split(rawId, "-")
+	id = Ids[0] + Ids[4]
+	return id
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,15 @@ module github.com/ipfans/go-common
 go 1.12
 
 require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/uuid v1.1.1
 	github.com/labstack/echo/v4 v4.1.5
+	github.com/pkg/errors v0.8.1 // indirect
 	github.com/stretchr/testify v1.3.0
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	go.uber.org/zap v1.10.0
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
